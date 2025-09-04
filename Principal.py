59be6ecb3da62f7afe225fad37d6a4d9da15097f
Alexander-Rico-Hostia <ricohostiaalexander@gmail.com>
2025-09-04 11:38:52 -0500

    he agragado el archivo prueba2

--- Principal.py
+++ Principal.py
@@ -12,9 +12,10 @@ class Persona:
 #persona1 -> objeto  Persona()->CLASE
 persona1=Persona()
 persona2=Persona()
+persona3=Persona()
 persona1.nombre="Alexander"
 persona2.nombre="Mercedes Maria Alexandra"
-
+persona3.nombre="Diana Esther"
 
 print(persona1.nombre)
 print(persona1.caminar())
